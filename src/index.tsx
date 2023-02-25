15471b071e1373b36ef1799110d905fea4a289ea
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-25 21:31:41 +0100

    Implement React Router

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
+import "@fontsource/signika-negative";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import App from "./App";
+import theme from "./styles/theme";
+import "./styles/index.css";
+import { router } from "./router/router";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
