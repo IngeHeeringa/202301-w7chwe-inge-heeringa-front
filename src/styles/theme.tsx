e3c94448b5b1505d37f8a9a35b5263aca9aec1f3
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-25 21:27:52 +0100

    Add global styles to app

---
+++ src/styles/theme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ba000d",
+    },
+    secondary: {
+      main: "#3a3a3a",
+    },
+  },
+});
+
+export default theme;
