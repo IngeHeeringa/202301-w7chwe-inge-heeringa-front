cfb06780a7e379b51d65a778cb67c30a7fbc94ea
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-25 22:52:06 +0100

    Create RegisterPage structure

---
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -0,0 +1,20 @@
+import RegisterForm from "../../components/RegisterForm/RegisterForm";
+import useUser from "../../hooks/useUser";
+
+const RegisterPage = (): JSX.Element => {
+  const { registerUser } = useUser();
+
+  const handleSubmit = (data: FormData) => {
+    registerUser(data);
+  };
+
+  return (
+    <section>
+      <h1>Sign up today</h1>
+      <span>Instantly connect with your friends and enemies</span>
+      <RegisterForm onSubmit={handleSubmit} />;
+    </section>
+  );
+};
+
+export default RegisterPage;
