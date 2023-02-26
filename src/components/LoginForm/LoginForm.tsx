8b9f7b68bee589b3cd054b3c32b8f16f60feb6df
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-26 20:36:21 +0100

    Add login functionality to LoginForm

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,14 +1,51 @@
 import { Box, Button, TextField } from "@mui/material";
+import { useState } from "react";
+import { UserCredentials } from "../../types";
 import styles from "./LoginForm.module.css";
 
-const LoginForm = (): JSX.Element => {
+interface LoginFormProps {
+  onSubmit: (userCredentials: UserCredentials) => void;
+  error: boolean;
+}
+
+const LoginForm = ({ onSubmit, error }: LoginFormProps): JSX.Element => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChangeUsername = (
+    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    setUsername(event.target.value);
+  };
+
+  const handleChangePassword = (
+    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setUsername("");
+    setPassword("");
+
+    const userCredentials = {
+      username,
+      password,
+    };
+
+    onSubmit(userCredentials);
+  };
+
   return (
-    <Box component="form" className={styles.loginForm}>
+    <Box component="form" className={styles.loginForm} onSubmit={handleSubmit}>
       <TextField
         required
         id="outlined-username-input"
         name="username"
         label="Username"
+        onChange={handleChangeUsername}
       />
       <TextField
         required
@@ -16,6 +53,7 @@ const LoginForm = (): JSX.Element => {
         label="Password"
         name="password"
         type="password"
+        onChange={handleChangePassword}
       />
       <Button variant="contained" type="submit">
         Log in
