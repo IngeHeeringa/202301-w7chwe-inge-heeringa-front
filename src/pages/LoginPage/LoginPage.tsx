1b3136992a558cb8e79d51c7aa1e512e0a5c1ad0
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-26 23:36:04 +0100

    Add conditional rendering of LoginForm based on user's logged state

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,12 +1,16 @@
+import { Button } from "@mui/material";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import useUser from "../../hooks/useUser";
+import { useAppSelector } from "../../store/hooks";
 import { UserCredentials } from "../../types";
 import styles from "./LoginPage.module.css";
 
 const LoginPage = (): JSX.Element => {
   const { loginUser } = useUser();
   const [error, setError] = useState(false);
+  const isLogged = useAppSelector((state) => state.user.isLogged);
 
   const handleSubmit = async (userCredentials: UserCredentials) => {
     try {
@@ -18,9 +22,27 @@ const LoginPage = (): JSX.Element => {
 
   return (
     <section className={styles.loginPage}>
-      <h1>Welcome back</h1>
-      <span>Log in to connect with your friends and enemies</span>
-      <LoginForm onSubmit={handleSubmit} error={error} />
+      {isLogged ? (
+        <>
+          <div className={styles.wrapper}>
+            <span className={styles.loginPage__welcomeMessage}>Hey there!</span>
+            <span className={styles.loginPage__successMessage}>
+              Your frenemies are only one click away
+            </span>
+          </div>
+          <Link to="/">
+            <Button variant="contained" size="large">
+              Go to home
+            </Button>
+          </Link>
+        </>
+      ) : (
+        <>
+          <h1>Welcome back</h1>
+          <span>Log in to connect with your friends and enemies</span>
+          <LoginForm onSubmit={handleSubmit} error={error} />
+        </>
+      )}
     </section>
   );
 };
