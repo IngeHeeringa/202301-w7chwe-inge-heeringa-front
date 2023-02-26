ac5db9db8c963637df726d119c24659a78f8e73b
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-27 00:40:59 +0100

    Test conditional rendering of LoginForm component

--- src/pages/LoginPage/LoginPage.test.tsx
+++ src/pages/LoginPage/LoginPage.test.tsx
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event/";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { userReducer } from "../../store/features/userSlice";
 import LoginPage from "./LoginPage";
+import { UserState } from "../../types";
 
 const store = configureStore({
   reducer: { user: userReducer },
@@ -16,7 +19,8 @@ describe("Given a LoginPage", () => {
       render(
         <Provider store={store}>
           <LoginPage />
-        </Provider>
+        </Provider>,
+        { wrapper: BrowserRouter }
       );
 
       const title = screen.getByRole("heading", { name: expectedTitle });
@@ -31,12 +35,49 @@ describe("Given a LoginPage", () => {
       render(
         <Provider store={store}>
           <LoginPage />
-        </Provider>
+        </Provider>,
+        { wrapper: BrowserRouter }
       );
 
       const subtitle = screen.getByText(expectedSubtitle);
 
       expect(subtitle).toBeInTheDocument();
+    });
+  });
+
+  describe("When the login form is submitted successfully", () => {
+    test("Then it should show a redirect button to the home page", async () => {
+      const mockUserStateTrue: UserState = {
+        username: "User",
+        token: "123",
+        isLogged: true,
+      };
+
+      jest.mock("../../store/hooks", () => ({
+        ...jest.requireActual("../../store/hooks"),
+        useAppSelector: () => mockUserStateTrue,
+      }));
+
+      render(
+        <Provider store={store}>
+          <LoginPage />
+        </Provider>,
+        { wrapper: BrowserRouter }
+      );
+
+      await userEvent.type(screen.getByLabelText(/username/i), "User");
+      await userEvent.type(screen.getByLabelText(/password/i), "user123");
+      await userEvent.click(screen.getByRole("button", { name: /log in/i }));
+
+      const redirectButton = await screen.findByRole(
+        "button",
+        {
+          name: /go to home/i,
+        },
+        { timeout: 3000 }
+      );
+
+      expect(redirectButton).toBeInTheDocument();
     });
   });
 });
